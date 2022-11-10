d659cd37f277c363ac0f5b4737525c1a97dbd3f7
azharrahad31 <azherudinrahad@gmail.com>
2022-11-10 17:23:07 +0600

    Fex error

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors= require('cors');
-//const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 const app =express();
 require('dotenv').config()
 
@@ -19,12 +19,74 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run(){
  try{
     const serviceCollection=client.db('products').collection('services');
+    const orderCollection=client.db('products').collection('order');
+    
+    app.get('/servicesHome',async(req,res)=>{
+        const query = {};
+        const cursor= serviceCollection.find(query);
+       const services= await cursor.limit(3).toArray();
+        res.send(services);
+       })
     app.get('/services',async(req,res)=>{
         const query = {};
         const cursor= serviceCollection.find(query);
        const services= await cursor.toArray();
         res.send(services);
        })
+
+       app.get('/services/:id',async(req,res)=> {
+        const id=req.params.id;
+        const query={_id: ObjectId(id)};
+        const service=await serviceCollection.findOne(query);
+        res.send(service);
+       })
+       app.delete('/orders/:id',async(req,res)=>{
+        
+        const id = req.params.id;
+        const query ={_id: ObjectId(id)};
+        const result =await orderCollection.deleteOne(query);
+        res.send(result)
+       })
+       app.patch('/orders/:id', async(req,res)=>{
+        const id =req.params.id;
+        console.log(id);
+        const status= req.body.status
+        const query ={_id:ObjectId(id)};
+        const updateDoc = {
+            $set:{
+
+                status: status
+
+            }
+        }
+        const result =await orderCollection.updateOne(query,updateDoc)
+        res.send(result);
+
+       })
+      app.get('/orders', async(req,res)=>{
+        const decoded =req.decoded;
+        console.log('inside orders api',decoded)
+        let query={};
+        if(req.query.email){
+            query={
+                email: req.query.email
+            }
+        }
+        const cursor=orderCollection.find(query);
+        const orders =await cursor.toArray();
+        res.send(orders)
+       })
+ 
+       app.post('/orders',async(req,res)=>{
+        const order = req.body;
+        const result = await orderCollection.insertOne(order);
+        res.send(result);
+
+       })
+
+   
+  
+
 
 
     
